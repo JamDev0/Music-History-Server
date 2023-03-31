b029420fd08195f7bc1b194b74d3b0baee8f1538
JamDev0 <jamdeveloper0@gmail.com>
2023-03-31 14:28:22 -0300

    Feature: initial transition to fastify

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,45 @@ import fastify from "fastify";
 //@ts-ignore
 const server = fastify();
 
+server.post("/musics", (req, res) => {  
+})
+
+server.patch("/musics", (req, res) => {
+
+})
+
+server.get("/musics", (req, res) => {
+})
+
+server.get("/musics/:id", (req, res) => {
+  
+})
+
+server.delete("/musics/:id", (req, res) => {
+  
+})
+
+server.get("/musics/:id/plays", (req, res) => {
+
+})
+
+server.post("/history", (req, res) => {
+
+})
+
+server.get("/history", (req, res) => {
+
+})
+
+server.get("/history/:id", (req, res) => {
+  
+})
+
+server.delete("/history/:id", (req, res) => {
+  
+})
+
+
 const port = 3333;
 
 server.listen({port})
