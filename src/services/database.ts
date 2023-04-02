54d90d2c3e854ae4275c7982a55c41e8cda0c406
JamDev0 <jamdeveloper0@gmail.com>
2023-04-02 18:36:13 -0300

    Feature: Migration from static connection to environment variable based connection

--- src/services/database.ts
+++ src/services/database.ts
@@ -1,14 +1,15 @@
 import configKnex, { Knex } from 'knex';
+import { env } from '../env';
 
 
 export const config: Knex.Config = {
-	client: 'sqlite',
-	connection: './db/tmp/app.db',
-	useNullAsDefault: true,
-	migrations: {
-		extension: 'ts',
-		directory: './db'
-	}
+  client: 'sqlite',
+  connection: env.DATABASE_URL,
+  useNullAsDefault: true,
+  migrations: {
+    extension: 'ts',
+    directory: './db'
+  }
 };
 
 export const knex = configKnex (config);
