2f13d62a6d81009ae6a61f197284b6ee60ffad5a
JamDev0 <jamdeveloper0@gmail.com>
2023-04-09 21:28:32 -0300

    Fix: wrong PORT .env variable sintax

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,8 @@ import { app } from './app';
 
 import { env } from './env';
 
-app.listen({port: env.SERVER_PORT})
+app.listen({port: env.PORT})
   .then(() => {
-    console.log(`Running 🏃‍♂️ on ${env.SERVER_PORT} my guy`);
+    console.log(`Running on ${env.PORT} my guy`);
   });
 
